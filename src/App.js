0b020b91e41e93d2da0eb029bbc24364c207f0eb
prayanshusharma <prayanshusharma7@gmail.com>
2021-05-27 23:17:22 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {CardList} from './components/card-list/card-list.components';
+class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      monsters:[
+      ]
+    };
+  }
+  componentDidMount(){
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(Response => Response.json()).then(users => this.setState({monsters: users}))
+  }
+  render(){
+    return (
+      <div className='App'>
+      <CardList monsters={this.state.monsters}></CardList>
+      </div>
+      
+    );
+}
+}
+export default App;
